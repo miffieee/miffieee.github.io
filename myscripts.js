174dfe8b6641eb52b81bfad0c1d5148231ddf753
miffieee <alisha-ahsila@hotmail.com>
2018-02-03 22:02:40 -0500

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,26 @@
+//Created by Alisha Zhang Feb 3, 2018
+
+// Dropdown menu function
+function dropDownMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropimg')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+// Scroll down button
+function scollDownButton() {
+		document.getElementById("downButton").onclick
+}
